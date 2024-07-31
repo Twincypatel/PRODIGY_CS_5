d4f54f9ea77d9764fb1915a48e357b8eec73c818
Twincypatel <104062892+Twincypatel@users.noreply.github.com>
2024-07-31 14:04:38 +0530

    Add files via upload

---
+++ network_packet_analyzer.py
@@ -0,0 +1,20 @@
+from scapy.all import sniff, IP, TCP, wrpcap, rdpcap
+
+def packet_callback(packet):
+    if IP in packet:
+        ip_src = packet[IP].src
+        ip_dst = packet[IP].dst
+        print(f"IP Source: {ip_src} -> IP Destination: {ip_dst}")
+        
+    if TCP in packet:
+        tcp_sport = packet[TCP].sport
+        tcp_dport = packet[TCP].dport
+        print(f"TCP Source Port: {tcp_sport} -> TCP Destination Port: {tcp_dport}")
+
+packets = sniff(filter="tcp", prn=packet_callback, count=10)
+wrpcap('captured_packets.pcap', packets)
+
+saved_packets = rdpcap('captured_packets.pcap')
+
+for packet in saved_packets:
+    print(packet.show())
